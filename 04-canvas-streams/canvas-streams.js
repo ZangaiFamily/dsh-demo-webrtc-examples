17a9bb86edd801174ec40df9125ade36d6e7b99c
matrixbirds <yooobuntu@gmail.com>
2019-04-11 23:29:26 +0800

    feat. add screen freeze

--- 04-canvas-streams/canvas-streams.js
+++ 04-canvas-streams/canvas-streams.js
@@ -7,72 +7,90 @@ var outputCtx = $( '.output-canvas canvas' )[ 0 ].getContext( '2d' );
 var width = 300;
 var height = 225;
 
-navigator.getUserMedia(
-	{ video: true, audio: true },
-	stream => {
-		localStream = stream;
-		$( '.local video' ).attr( 'src', URL.createObjectURL( stream ) );
-		drawToCanvas();
-		init();
-	},
-	error => {
-		alert( 'error while accessing usermedia ' + error.toString() );
-	}
-);
+$(() => {
 
-function drawToCanvas() {
-	// draw video to input canvas
-	inputCtx.drawImage( localVideo, 0, 0, width, height );
+	window.start = true
 
-	// get pixel data from input canvas
-	var pixelData = inputCtx.getImageData( 0, 0, width, height );
-
-	var avg, i;
-
-	// simple greyscale transformation
-	for( i = 0; i < pixelData.data.length; i += 4 ) {
-		avg = ( pixelData.data[ i ] + pixelData.data[ i + 1 ] + pixelData.data[ i + 2 ] ) / 3;
-		pixelData.data[ i ] = avg;
-		pixelData.data[ i + 1 ] = avg;
-		pixelData.data[ i + 2 ] = avg;
-	}
-
-	outputCtx.putImageData( pixelData, 0, 0 );
-	requestAnimationFrame( drawToCanvas );
-}
-
-ds = deepstream( 'wss://013.deepstreamhub.com?apiKey=d02f8752-7d26-4cf0-965a-90c21536410f' );
-
-ds.login( null, init );
-
-function init() {
-	if( !localStream || ds.getConnectionState() !== 'OPEN' || isInitialized ) {
-		return;
-	}
-
-	isInitialized = true;
-
-	const userName = 'user/' + ds.getUid();
-
-	const p2pConnection = new SimplePeer({
-		initiator: document.location.hash === '#initiator',
-		stream: $( '.output-canvas canvas' )[ 0 ].captureStream()
-	});
-
-	p2pConnection.on( 'signal', signal => {
-		ds.event.emit( 'rtc-signal-03', {
-			sender: userName,
-			signal: signal
-		});
-	});
-
-	ds.event.subscribe( 'rtc-signal-03', msg => {
-		if( msg.sender !== userName ) {
-			p2pConnection.signal( msg.signal );
+	navigator.getUserMedia(
+		{ video: { facingMode: { ideal: 'environment' } }, audio: false },
+		stream => {
+			localStream = stream;
+			$( '.local video' )[0].srcObject = stream;
+			drawToCanvas();
+			// init();
+		},
+		error => {
+			alert( 'error while accessing usermedia ' + error.toString() );
 		}
-	});
+	);
+	
+	function drawToCanvas() {
+		if (window.start) {
+			// draw video to input canvas
+			inputCtx.drawImage( localVideo, 0, 0, width, height );
+		
+			// get pixel data from input canvas
+			var pixelData = inputCtx.getImageData( 0, 0, width, height );
+		
+			var avg, i;
+		
+			// simple greyscale transformation
+			for( i = 0; i < pixelData.data.length; i += 4 ) {
+				avg = ( pixelData.data[ i ] + pixelData.data[ i + 1 ] + pixelData.data[ i + 2 ] ) / 3;
+				pixelData.data[ i ] = avg;
+				pixelData.data[ i + 1 ] = avg;
+				pixelData.data[ i + 2 ] = avg;
+			}
+		
+			outputCtx.putImageData( pixelData, 0, 0 );
+			requestAnimationFrame( drawToCanvas );
+		}
+	}
 
-	p2pConnection.on( 'stream', remoteStream => {
-		$( '.remote video' ).attr( 'src', URL.createObjectURL( remoteStream ) );
-	});
-}
+	$("#pause").on("click", () => {
+		$(".local video")[0]
+			.srcObject
+			.getTracks().forEach(t => t.enabled = !t.enabled);
+			window.start = !window.start
+			if (window.start) {
+				drawToCanvas();
+			}
+	})
+})
+
+
+// ds = deepstream( 'wss://013.deepstreamhub.com?apiKey=d02f8752-7d26-4cf0-965a-90c21536410f' );
+
+// ds.login( null, init );
+
+// function init() {
+// 	if( !localStream || ds.getConnectionState() !== 'OPEN' || isInitialized ) {
+// 		return;
+// 	}
+
+// 	isInitialized = true;
+
+// 	const userName = 'user/' + ds.getUid();
+
+// 	const p2pConnection = new SimplePeer({
+// 		initiator: document.location.hash === '#initiator',
+// 		stream: $( '.output-canvas canvas' )[ 0 ].captureStream()
+// 	});
+
+// 	p2pConnection.on( 'signal', signal => {
+// 		ds.event.emit( 'rtc-signal-03', {
+// 			sender: userName,
+// 			signal: signal
+// 		});
+// 	});
+
+// 	ds.event.subscribe( 'rtc-signal-03', msg => {
+// 		if( msg.sender !== userName ) {
+// 			p2pConnection.signal( msg.signal );
+// 		}
+// 	});
+
+// 	p2pConnection.on( 'stream', remoteStream => {
+// 		$( '.remote video' ).attr( 'src', URL.createObjectURL( remoteStream ) );
+// 	});
+// }
